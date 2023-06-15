6167e43dfc95bd5dff3f5cf977faf41d07252887
Ankush Singal <andysingal@gmail.com>
2023-06-15 23:08:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ def main():
     return {"data": destination_table.num_rows}
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5052)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8082)))
